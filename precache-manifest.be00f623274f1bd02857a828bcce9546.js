25567563f0a9a501d00056ec4837ad1902d3e704
GabyMarapi <gaby1017x@gmail.com>
2018-10-24 18:22:04 -0500

    Updates

---
+++ precache-manifest.be00f623274f1bd02857a828bcce9546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "0fa188561ea2552e37bb",
+    "url": "/lim-2018-01-burger-queen/static/js/main.0fa18856.chunk.js"
+  },
+  {
+    "revision": "ec275888fb9db865de15",
+    "url": "/lim-2018-01-burger-queen/static/js/1.ec275888.chunk.js"
+  },
+  {
+    "revision": "0fa188561ea2552e37bb",
+    "url": "/lim-2018-01-burger-queen/static/css/main.7648145c.chunk.css"
+  },
+  {
+    "revision": "e15586432d9d48fd1918ff4321fc168a",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
